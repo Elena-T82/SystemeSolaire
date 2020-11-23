a9d7a64ffafed0d80a90a99577d59ffe35865c62
Elena TASQUIER <elena-tasquier@hotmail.fr>
2020-11-23 12:00:29 +0100

    Push fin de projet

---
+++ SystemeSolaire/Assets/Scripts/ScriptControleur.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScriptControleur : MonoBehaviour
+{
+
+    /// <summary>
+    /// Mouvement de la terre
+    /// </summary>
+    public float alphaTerre = 0f;
+
+    /// <summary>
+    /// Mouvement de la lune
+    /// </summary>
+    public float alphaLune = 0f;
+
+    /// <summary>
+    /// Mouvemenet de Vénus
+    /// </summary>
+    public float alphaVenus = 0f;
+
+    /// <summary>
+    /// Mouvement de Mars
+    /// </summary>
+    public float alphaMars = 0f;
+
+    /// <summary>
+    /// Vitesse de la terre
+    /// </summary>
+    public float vitesseTerre = 0.01f;
+
+    /// <summary>
+    /// Vitesse de la lune
+    /// </summary>
+    public float vitesseLune = 0.02f;
+
+    /// <summary>
+    /// Vitesse de Vénus
+    /// </summary>
+    public float vitesseVenus = 0.005f;
+
+    /// <summary>
+    /// Vitesse de Mars
+    /// </summary>
+    public float vitesseMars = 0.009f;
+
+
+    public float decentralisation = 10f;
+
+    public GameObject soleil;
+    public GameObject terre;
+    public GameObject lune;
+    public GameObject mars;
+    public GameObject venus;
+
+    public Vector3 centreSoleil;
+    public Vector3 centreTerre;
+
+    public GameObject vaisseau;
+    public Text positionJoueur;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        centreSoleil = new Vector3(soleil.transform.position.x + decentralisation, 0, soleil.transform.position.z);
+        centreTerre = terre.transform.position;
+
+        terre.transform.Rotate(0.2f, 0.2f, 0, Space.World);
+        venus.transform.Rotate(0.2f, 0, 0.2f, Space.World);
+        mars.transform.Rotate(0.2f, 0, 0.2f, Space.World);
+
+
+        terre.transform.position = new Vector3(centreSoleil.x + 45 * Mathf.Cos(alphaTerre), centreSoleil.y + 40 * Mathf.Sin(alphaTerre), 0);
+        lune.transform.position = new Vector3(centreTerre.x + 10 * Mathf.Cos(alphaLune), centreTerre.y + 10 * Mathf.Sin(alphaLune), 0.0f);
+
+
+        mars.transform.position = new Vector3(centreSoleil.x + 60 * Mathf.Cos(alphaMars), 0, centreSoleil.z + 55 * Mathf.Sin(alphaMars));
+        venus.transform.position = new Vector3(centreSoleil.x + 30 * Mathf.Cos(alphaVenus), 0, centreSoleil.z + 35 * Mathf.Sin(alphaVenus));
+
+
+        alphaTerre += vitesseTerre;
+        alphaMars += vitesseMars;
+        alphaVenus += vitesseVenus;
+        alphaLune += vitesseLune;
+
+
+        positionJoueur.text = "Position vaisseau : X(" + Math.Round(vaisseau.transform.position.x, 2) + ") Y( " + Math.Round(vaisseau.transform.position.y, 2) + ") Z(" + Math.Round(vaisseau.transform.position.z, 2) + ")";
+    }
+}
---
+++ SystemeSolaire/Assets/Scripts/Vaisseau.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vaisseau : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float vitesse = 10f;
+    public Vector3 moveDirection = Vector3.zero;
+
+    public Camera CameraAvant;
+    public Camera CameraArriere;
+    public Camera CameraExterieur;
+    public Camera Camera3emePersonne;
+
+    public GameObject lumiere1;
+    public GameObject lumiere2;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //-------------Les déplacements--------------------
+
+
+        //Avancer, reculer, rotation droite et gauche
+
+        if (Input.GetKey(KeyCode.Z)) //GetKey : tant que la key est appuyée.
+        {
+            moveDirection = new Vector3(0, 0, -0.7f) * vitesse * Time.deltaTime;
+            transform.position += transform.TransformDirection(moveDirection); //permet d'avancer en prenant en compte la rotation
+
+            lumiere1.SetActive(true);
+            lumiere2.SetActive(true);
+        }
+        if (Input.GetKeyUp(KeyCode.Z)) //si la key est relaché
+        {
+            lumiere1.SetActive(false);
+            lumiere2.SetActive(false);
+        }
+
+
+        if (Input.GetKey(KeyCode.Q)) //GetKey : tant que la key est appuyée.
+        {
+            transform.Rotate (- Vector3.up * vitesse * 10 * Time.deltaTime); //rotation gauche
+
+            lumiere1.SetActive(true);
+            lumiere2.SetActive(true);
+        }
+        if (Input.GetKeyUp(KeyCode.Q)) //si la key est relaché
+        {
+            lumiere1.SetActive(false);
+            lumiere2.SetActive(false);
+        }
+
+
+        if (Input.GetKey(KeyCode.S)) //GetKey : tant que la key est appuyée.
+        {
+            moveDirection = new Vector3(0, 0, 0.7f) * vitesse * Time.deltaTime;
+            transform.position += transform.TransformDirection(moveDirection);
+
+            lumiere1.SetActive(true);
+            lumiere2.SetActive(true);
+        }
+        if (Input.GetKeyUp(KeyCode.S)) //si la key est relaché
+        {
+            lumiere1.SetActive(false);
+            lumiere2.SetActive(false);
+        }
+
+
+        if (Input.GetKey(KeyCode.D)) //GetKey : tant que la key est appuyée.
+        {
+            transform.Rotate (Vector3.up * (Input.GetAxis("Horizontal") * vitesse * 10 * Time.deltaTime)); //rotation droite
+
+            lumiere1.SetActive(true);
+            lumiere2.SetActive(true);
+        }
+        if (Input.GetKeyUp(KeyCode.D)) //si la key est relaché
+        {
+            lumiere1.SetActive(false);
+            lumiere2.SetActive(false);
+        }
+
+
+        //Monter ou descendre
+
+
+        if (Input.GetKey(KeyCode.Space)) //Permet de monter
+        {
+            transform.position += new Vector3(0, 0.7f, 0) * vitesse * Time.deltaTime;
+
+            lumiere1.SetActive(true);
+            lumiere2.SetActive(true);
+        }
+        if (Input.GetKeyUp(KeyCode.Space)) //si la key est relaché
+        {
+            lumiere1.SetActive(false);
+            lumiere2.SetActive(false);
+        }
+
+
+        if (Input.GetKey(KeyCode.LeftShift)) //Permet de descendre
+        {
+            transform.position += new Vector3(0, -0.7f, 0) * vitesse * Time.deltaTime;
+
+            lumiere1.SetActive(true);
+            lumiere2.SetActive(true);
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift)) //si la key est relaché
+        {
+            lumiere1.SetActive(false);
+            lumiere2.SetActive(false);
+        }
+
+
+        //---------------Les caméras------------------------
+
+
+
+        if (Input.GetKeyDown(KeyCode.E)) //Si la key est appuyé une fois
+        {
+            if (CameraAvant.depth == 1) //Permet de basculer sur la caméra arriere
+            {
+                CameraArriere.depth = 1;
+                CameraAvant.depth = 0;
+                CameraExterieur.depth = 0;
+            }
+            else                        //Permet de basculer sur la caméra avant
+            {
+                CameraAvant.depth = 1;
+                CameraArriere.depth = 0;
+                CameraExterieur.depth = 0;
+
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.R)) //Permet de basculer sur la caméra de la 3eme personne
+        {
+            Camera3emePersonne.depth = 1;
+            CameraAvant.depth = 0;
+            CameraArriere.depth = 0;
+            CameraExterieur.depth = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A)) //Si la key est appuyé une fois
+        {
+            if (CameraExterieur.depth == 0) //Permet de basculer sur la caméra exterieur
+            {
+                CameraExterieur.depth = 1;
+                CameraAvant.depth = 0;
+                CameraArriere.depth = 0;
+            }
+        }
+    }
+}
